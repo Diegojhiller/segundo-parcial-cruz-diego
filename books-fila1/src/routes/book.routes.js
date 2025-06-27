623d620b19605bd1ec3d15ff5035b4835eed680f
Diego Cruz <diegohiller754@gmail.com>
2025-06-27 11:37:21 -0300

    cfeacion de las rutas CRUD de los libros

---
+++ books-fila1/src/routes/book.routes.js
@@ -0,0 +1,18 @@
+import express from "express";
+import {
+    createBook,
+    getAllBook,
+    getBookById, 
+    updateBook,
+    deleteBook,
+} from "../controllers/character.controllers.js";
+
+const routes = express.Router();
+
+routes.get("/" , getAllBook);
+routes.get("/:id", getBookById);
+routes.post("/" , createBook);
+routes.put("/:id" , updateBook);
+routes.delete("/:id" , deleteBook);
+
+export default routes;
